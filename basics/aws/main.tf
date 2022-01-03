eb6cdfc444032f8faf9ede1918b4eba49b1026ba
juliosouza-fl <julio.souza@fountainledge.com>
2022-01-03 17:22:17 -0300

    vars

--- basics/aws/main.tf
+++ basics/aws/main.tf
@@ -1,20 +1,28 @@
 provider "aws" {
-  access_key = "key"
-  secret_key = "secret"
-  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
 
 resource "aws_vpc" "dev-vpc-01" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.subnet_cidr_blocks[0].cidr_block
   tags = {
-    Name = "terraform-vpc-dev-01"
+    Name = var.subnet_cidr_blocks[0].name
   }
+}
+
+output "dev-vpc-01-output" {
+  value = aws_vpc.dev-vpc-01.id
 }
 
 resource "aws_subnet" "dev-subnet-01" {
   vpc_id     = aws_vpc.dev-vpc-01.id
-  cidr_block = "10.0.10.0/24"
+  cidr_block = var.subnet_cidr_blocks[1].cidr_block
   tags = {
-    Name = "terraform-dev-subnet-01"
+    Name = var.subnet_cidr_blocks[1].name
   }
+}
+
+output "dev-subnet-01-output" {
+  value = aws_subnet.dev-subnet-01.id
 }
